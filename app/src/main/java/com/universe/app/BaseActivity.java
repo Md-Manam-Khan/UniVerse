9b94fbc5ac459a2c6157afacbcb4830705d745ab
Md-Manam-Khan <manamkhan921@gmail.com>
2026-09-23 00:09:17 +0600

    Code fixed

--- app/src/main/java/com/universe/app/BaseActivity.java
+++ app/src/main/java/com/universe/app/BaseActivity.java
@@ -1,44 +1,40 @@
 package com.universe.app;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-public abstract class BaseActivity extends AppCompatActivity {
-
+public abstract class BaseActivity extends AppCompatActivity 
+{
     protected DarkModeManager darkModeManager;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         darkModeManager = new DarkModeManager(this);
         darkModeManager.applySavedMode();
         super.onCreate(savedInstanceState);
     }
-
     @Override
-    public void onPostCreate(Bundle savedInstanceState) {
+    public void onPostCreate(Bundle savedInstanceState) 
+    {
         super.onPostCreate(savedInstanceState);
         applyBlackBackgroundIfDark();
         applyKeyboardResize();
     }
-
-    private void applyKeyboardResize() {
+    private void applyKeyboardResize() 
+    {
         ViewGroup content = findViewById(android.R.id.content);
-        if (content == null || content.getChildCount() == 0) {
+        if (content == null || content.getChildCount() == 0) 
+        {
             return;
         }
-
         View root = content.getChildAt(0);
         int initialPaddingBottom = root.getPaddingBottom();
         int initialPaddingLeft = root.getPaddingLeft();
         int initialPaddingTop = root.getPaddingTop();
         int initialPaddingRight = root.getPaddingRight();
-
         ViewCompat.setOnApplyWindowInsetsListener(root, (v, insets) -> {
             int imeHeight = insets.getInsets(WindowInsetsCompat.Type.ime()).bottom;
             int navHeight = insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom;
@@ -48,32 +44,31 @@ public abstract class BaseActivity extends AppCompatActivity {
         });
         ViewCompat.requestApplyInsets(root);
     }
-
-    private void applyBlackBackgroundIfDark() {
-        if (!darkModeManager.isDarkMode()) {
+    private void applyBlackBackgroundIfDark() 
+    {
+        if (!darkModeManager.isDarkMode()) 
+        {
             return;
         }
-
         ViewGroup content = findViewById(android.R.id.content);
-        if (content == null || content.getChildCount() == 0) {
+        if (content == null || content.getChildCount() == 0) 
+        {
             return;
         }
-
         getWindow().getDecorView().setBackgroundColor(Color.BLACK);
-
         View root = content.getChildAt(0);
         root.setBackgroundColor(Color.BLACK);
-
-        if (root instanceof ViewGroup) {
+        if (root instanceof ViewGroup) 
+        {
             ViewGroup rootGroup = (ViewGroup) root;
-            for (int i = 0; i < rootGroup.getChildCount(); i++) {
+            for (int i = 0; i < rootGroup.getChildCount(); i++) 
+            {
                 View child = rootGroup.getChildAt(i);
                 boolean isPlainView = child.getClass() == View.class;
                 ViewGroup.LayoutParams lp = child.getLayoutParams();
-                boolean isFullScreen = lp != null
-                        && lp.width == ViewGroup.LayoutParams.MATCH_PARENT
-                        && lp.height == ViewGroup.LayoutParams.MATCH_PARENT;
-                if (isPlainView && isFullScreen) {
+                boolean isFullScreen = lp != null && lp.width == ViewGroup.LayoutParams.MATCH_PARENT && lp.height == ViewGroup.LayoutParams.MATCH_PARENT;
+                if (isPlainView && isFullScreen) 
+                {
                     child.setBackgroundColor(Color.BLACK);
                 }
             }
